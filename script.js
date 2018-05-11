d1e22c09948120a91783b7b55ed037cdca67d0c7
tiffanydemps <35701572+tiffanydemps@users.noreply.github.com>
2018-05-11 09:22:47 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+        //add and remove at the same time
+
+      //   $("#show").addClass( "findouthow" ).removeClass( "checkagainlater" )
+      // });
+
+
+       $(".box").click(function(){
+            $(".box").hide();
+        });
+
+
+
+$("#replacetext").hover(function(){
+$("#replace").html("<h1>Be a happy worker!</h1>");
+});
+
+
+$("#thanks").hover(function(){
+  $(".bottom").hide();
+});
+
+
+
+});
